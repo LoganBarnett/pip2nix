1a0a112bde7b513f45a435f0b6b65107a4506c01
Logan Barnett <logustus@gmail.com>
2024-03-05 20:56:55 -0800

    use a supported Python for docs

--- release.nix
+++ release.nix
@@ -34,7 +34,7 @@ let
 
     docs = pkgs.stdenv.mkDerivation {
       name = "pip2nix-docs";
-      src = pip2nix.python36.src;
+      src = pip2nix.python39.src;
       #outputs = [ "html" ];  # TODO: PDF would be even nicer on CI
       buildInputs = [ pip2nix.python36 ] ++ (with  pkgs.python36Packages; [
         sphinx
